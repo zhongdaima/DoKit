af0b3fafb2c2892f40c927387ec3382daa02baf2
wbx <zhongdaima@163.com>
2025-07-07 11:02:49 +0800

    Update DoraemonJavaScriptManager.m
    删除UIWebView相关内容

--- iOS/DoraemonKit/Src/Core/Plugin/Common/JavaScript/Function/DoraemonJavaScriptManager.m
+++ iOS/DoraemonKit/Src/Core/Plugin/Common/JavaScript/Function/DoraemonJavaScriptManager.m
@@ -77,13 +77,6 @@
             }
         }];
     }
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    if ([currentWebView isKindOfClass:UIWebView.class]) {
-        UIWebView *webView = currentWebView;
-        [webView stringByEvaluatingJavaScriptFromString:script];
-    }
-#pragma clang diagnostic pop
 }
 
 @end
